ed59e2de75472d0b20c84c8650b1822b0f351f25
Mahmoud Kamal <mk.bayumi@gmail.com>
2024-06-30 06:37:00 +0300

    Basic Implementation
    building a very simple Promise class, not supporing async operations just immediate execution of handlers passed to then and catch functions

---
+++ README.md
@@ -0,0 +1,4 @@
+# Promises (Custom implementation)
+
+> [!NOTE] Reinventing the gears
+> I got confused lots of time with this concept so I will try to reinvent the gears and impelement it myself.
--- index.js
+++ index.js
@@ -1,5 +1,52 @@
 "use strict"
 
-class Promise {
+class CustomPromise {
+  #state;
+  #result;
+  constructor(handler) {
+    this.#state = 'pending'; // fulfilled, rejected
+    this.#result = undefined
 
+    const resolve = (value) => {
+      if (this.#state === 'pending') {
+        this.#state = 'fulfilled';
+        this.#result = value;
+      }
+    }
+
+    const reject = (value) => {
+      if (this.#state === 'pending') {
+        this.#state = 'rejected';
+        this.#result = value;
+      }
+    }
+
+    try {
+      handler(resolve, reject);
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
+
+  then(onFulfilled, onRejected) {
+    try {
+      if (this.#state === 'fulfilled')
+        onFulfilled(this.#result);
+      else if (this.#state === 'rejected') {
+        onRejected(this.#result);
+      }
+    }
+    catch (err) {
+      console.log(err.message);
+    }
+  }
+
+  catch(onRejected) {
+    try {
+      onRejected(this.#result);
+    }
+    catch (err) {
+      console.log(err.message);
+    }
+  }
 }
